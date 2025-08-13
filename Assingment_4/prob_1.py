0420d8c0d9e29fd88697b8994bd9d4560385d76f
MS-Brothers <118957586+MS-Brothers@users.noreply.github.com>
2025-08-13 08:50:16 +0530

    Delete Assingment_4 directory

--- Assingment_4/prob_1.py
+++
@@ -1,7 +0,0 @@
-#WAP to print even number unitl n
-n=int(input('Enter the number : '))
-for i in range(2,n+1):
-    if(i%2==0):
-        print(f'{i}')
-
-        
--- Assingment_4/prob_3.py
+++
@@ -1,5 +0,0 @@
-#WAP to print sum of series upto n
-n=int(input('Enter the Number : '))
-for i in range(1,n+1):
-    s=i*(i+1)//2
-    print(f'{s}')
