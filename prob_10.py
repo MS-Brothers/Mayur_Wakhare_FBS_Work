63d7b5839c64358df5b65c5a8218a25dee5bcd67
MS-Brothers <118957586+MS-Brothers@users.noreply.github.com>
2025-08-15 15:37:02 +0530

    Delete prob_10.py

--- prob_10.py
+++
@@ -1,8 +0,0 @@
-#Write a program to check if person is eligible to marry or not (male age >=21 and female age>=18)
-gen=(input('Enter your gender : '))
-age=int(input('Enter your age : '))
-
-if(gen == 'male' and age >=21) or (gen=='female' and age >=18):
-    print(f'You are eligible')
-else:
-    print(f'You are not eligible')    
