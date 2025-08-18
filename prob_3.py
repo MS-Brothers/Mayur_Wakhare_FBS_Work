45755e6e4efd2f968dcac99620bf8276f0c362d4
MS-Brothers <118957586+MS-Brothers@users.noreply.github.com>
2025-08-18 13:42:07 +0530

    Delete prob_3.py

--- prob_3.py
+++
@@ -1,9 +0,0 @@
-#Write a program to input angles of a triangle and check whether triangle is valid or not.
-a=int(input('Enter the first angle : '))
-b=int(input('Enter the second angle : '))
-c=int(input('Enter the third angle : '))
-
-if(a+b+c == 180):
-    print(f'It is valid triangle')
-else:
-    print(f'It is not valid triangle')    
