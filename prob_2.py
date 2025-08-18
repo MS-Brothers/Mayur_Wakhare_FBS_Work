4ac77eca382240b768a726673508875791d26c58
MS-Brothers <118957586+MS-Brothers@users.noreply.github.com>
2025-08-18 13:41:53 +0530

    Delete prob_2.py

--- prob_2.py
+++
@@ -1,8 +0,0 @@
-# Write a program to input any alphabet and check whether it is vowel or consonant.
-l1=input('Enter the letter : ')
-if(l1 in 'aeiouAEIOU'):
-    print(f'It is a vowel')
-else:
-    print(f'It is a constant')   
-
-# Here is a "in" concept is important suppose we find a letter in string then we use   
